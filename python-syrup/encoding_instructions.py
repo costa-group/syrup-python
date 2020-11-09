b2e7b8f363ac7e4c00eaf41ca58be015b9ccb123
alexcere <48130030+alexcere@users.noreply.github.com>
2020-11-10 00:13:38 +0100

    Fixed minor bugs in instructions encoding

--- python-syrup/encoding_instructions.py
+++ python-syrup/encoding_instructions.py
@@ -67,12 +67,16 @@ def _non_comm_function_encoding(j, bs, o, r, theta_f):
     right_term_first_and = ["true"]
     # Second and can be empty, so we initialize terms to true value
     right_term_second_and = ["true"]
+    right_term_third_and = ["true"]
     for i in range(0, n):
         right_term_first_and.append(add_and(u(i,j), add_eq(x(i,j), o[i])))
     for i in range(bs-n+1, bs):
         right_term_second_and.append(add_not(u(i, j+1)))
+    for i in range(bs+n-1, bs):
+        right_term_third_and.append(add_not(u(i, j)))
     right_term = add_and(add_and(*right_term_first_and), u(0, j+1) , add_eq(x(0,j+1), r),
-                          move(j, n, min(bs-2+n, bs-1), 1-n) , add_and(*right_term_second_and))
+                          move(j, n, min(bs-2+n, bs-1), 1-n) , add_and(*right_term_second_and),
+                         add_and(*right_term_third_and))
     write_encoding(add_assert(add_implies(left_term, right_term)))
 
 
@@ -94,7 +98,7 @@ def _comm_function_encoding(j, bs, o0, o1, r, theta_f):
     left_term = add_eq(t(j), theta_f)
     right_term = add_and(u(0,j), u(1,j), add_or(add_and(add_eq(x(0,j), o0), add_eq(x(1,j), o1)),
                                                   add_and(add_eq(x(0,j), o1), add_eq(x(1,j), o0))),
-                          u(0,j+1), add_eq(x(0,j+1), r), move(j, 2, bs-1, -1), add_not(u(bs-1, j)))
+                          u(0,j+1), add_eq(x(0,j+1), r), move(j, 2, bs-1, -1), add_not(u(bs-1, j+1)))
     write_encoding(add_assert(add_implies(left_term, right_term)))
 
 
