58398fbfa0f49f6ac6caf2e365baedb8c9acc665
alexcere <48130030+alexcere@users.noreply.github.com>
2020-11-27 12:47:57 +0100

    Update tests with PUSH

--- tests/encoding_utils_tests.py
+++ tests/encoding_utils_tests.py
@@ -8,7 +8,7 @@ class Test_Encoding_Utils(unittest.TestCase):
                             'INSTR_2': [('INSTR_0', -1), ('PUSH', 7), ('INSTR_1', -1)],
                             'INSTR_0': [('PUSH', 5), ('PUSH', 6)], 'INSTR_1': [],
                             'INSTR_3': [('INSTR_2', -1), ('PUSH', 8), ('INSTR_2', -1)]}
-        expected_output = {'INSTR_0': 2, 'INSTR_1': 0, 'INSTR_2': 5, 'INSTR_3': 8, 'INSTR_4': 11}
+        expected_output = {'INSTR_0': 2, 'INSTR_1': 0, 'INSTR_2': 5, 'INSTR_3': 8, 'INSTR_4': 11, 'PUSH': 0}
         self.assertDictEqual(expected_output, generate_number_of_previous_instr_dict(dependency_graph))
 
     def test_prev_instructions_several_maximal_elements(self):
@@ -19,7 +19,20 @@ class Test_Encoding_Utils(unittest.TestCase):
                             'INSTR_5': [('INSTR_3', -1), ('INSTR_3', -1), ('INSTR_2', -1)],
                             'INSTR_6': [('INSTR_4', -1), ('INSTR_5', -1), ('INSTR_2', -1)]}
         expected_output = {'INSTR_0': 2, 'INSTR_1': 0, 'INSTR_2': 5, 'INSTR_3': 8,
-                           'INSTR_4': 11, 'INSTR_5': 11, 'INSTR_6': 17}
+                           'INSTR_4': 11, 'INSTR_5': 11, 'INSTR_6': 17, 'PUSH': 0}
+        self.assertDictEqual(expected_output, generate_number_of_previous_instr_dict(dependency_graph))
+
+
+    def test_prev_instructions_several_dependencies(self):
+        dependency_graph = {'INSTR_0': [('PUSH', 5), ('PUSH', 6)], 'INSTR_1': [],
+                            'INSTR_2': [('INSTR_0', -1), ('PUSH', 7), ('INSTR_1', -1)],
+                            'INSTR_3': [('INSTR_2', -1), ('PUSH', 8), ('INSTR_2', -1)],
+                            'INSTR_4': [('INSTR_3', -1), ('INSTR_1', -1), ('INSTR_2', -1)],
+                            'INSTR_5': [('INSTR_3', -1), ('INSTR_3', -1), ('INSTR_2', -1)],
+                            'INSTR_6': [('INSTR_4', -1), ('INSTR_5', -1), ('INSTR_2', -1)],
+                            'INSTR_7': [('PUSH', 3), ('PUSH', 7), ('INSTR_2', -1)]}
+        expected_output = {'INSTR_0': 2, 'INSTR_1': 0, 'INSTR_2': 5, 'INSTR_3': 8,
+                           'INSTR_4': 11, 'INSTR_5': 11, 'INSTR_6': 17, 'INSTR_7': 8, 'PUSH': 0}
         self.assertDictEqual(expected_output, generate_number_of_previous_instr_dict(dependency_graph))
 
 
