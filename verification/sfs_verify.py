ec5c3c20e156df463c9cec7efce2ae6270a2e259
Pablo <tutugordillo@gmail.com>
2020-12-14 20:22:36 +0100

    verification in progress

--- verification/sfs_verify.py
+++ verification/sfs_verify.py
@@ -1,7 +1,56 @@
-from oyente_ethir import analyze_isolate_block
+import sys
 import os
+sys.path.append(os.path.dirname(os.path.realpath(__file__))+"/../ethir")
+from utils_verify import *
+import rbr_isolate_block
+from utils import process_isolate_block
 
 
-def verify_sfs(sfs_dict):
-    pass
+costabs_path = "/tmp/costabs/"
+solutions_path = costabs_path + "solutions/"
+
+
+def verify_sfs(source, sfs_dict):
+
+    report = ""
     
+    if sfs_dict != {}:
+        if not os.path.exists(solutions_path):
+            print("[ERROR] Path "+solutions_path+" does not exist.")
+        else:
+            solution_files = filter(lambda x: x.find("disasm_opt")!=-1,os.listdir(solutions_path))
+            for f in solution_files:
+
+                block_id = get_block_id(f)
+                if os.path.getsize(solutions_path+f)!=0:
+                    
+                    input_stack = len(sfs_dict[block_id]["src_ws"])
+                    print("**********")
+                    print(input_stack)
+                    x, y = process_isolate_block(solutions_path+f, input_stack)
+
+                    block_data = {}
+                    block_data["instructions"] = x
+                    block_data["input"] = y
+
+                    print(block_data)
+                
+                    cname_aux = source.split("/")[-1]
+                    cname = cname_aux.strip().split(".")[0]
+    
+                    exit_code = rbr_isolate_block.evm2rbr_compiler(contract_name = cname, ebso = True, block = block_data)
+
+                else:
+                    report += "File for "+block_id+" is empty."
+                
+    else:
+        print("There are not solutions generated. They cannot be verified.")
+
+
+
+
+        
+
+
+if __name__ == '__main__':
+    verify_sfs({'block0':'prueba'})
---
+++ verification/utils_verify.py
@@ -0,0 +1,15 @@
+
+'''
+It takes the name of a file containing the optimized version of a
+block and extracts the id of the block from the name of the file.
+'''
+
+def get_block_id(file_name):
+
+    p = file_name.find("block")
+    end = file_name.find("_",p)
+
+    block_id = file_name[p:end]
+
+    print(block_id)
+    return block_id
