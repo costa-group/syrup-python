1eeb8c509f69fb0b7fb180a90b00089dd4fe4e81
alexcere <48130030+alexcere@users.noreply.github.com>
2021-04-24 16:42:27 +0200

    Added solver verify solution (yet to test)

---
+++ verification/solver_solution_verify.py
@@ -0,0 +1,44 @@
+import re
+
+
+# Given a sequence of theta opcodes tj and the corresponding aj, generates a dict for storing
+# pairs (tj, aj). If tj != theta(PUSH), then aj = -1. Note that we add at most one tj = theta(NOP),
+# as from that point the encoding guarantees that the remaining assignments in tj are also theta(NOP).
+def generate_dict_with_instructions(theta_sequence, pushed_values):
+    json_dict = dict()
+    ordered_theta_sequence = {k: v for k, v in sorted(theta_sequence.items(), key=lambda item: item[0])}
+    for pos, theta_value in ordered_theta_sequence.items():
+        # 0 is theta("PUSH") see generate_stack_theta in encoding_utils
+        if theta_value == 0:
+            aj = pushed_values[pos]
+        else:
+            aj = -1
+        json_dict[pos] = (theta_value, aj)
+
+        # 2 == theta("NOP")
+        # As soon as we find the first nop, we return the sequence
+        if theta_value == 2:
+            return json_dict
+    return json_dict
+
+
+# Generates a dict containing info to transform into a json
+def generate_solution_dict(solver_output):
+    pushed_values_decimal = {}
+    theta_sol = {}
+
+    pattern1 = re.compile("t_([0-9]*) ([0-9]*)")
+    pattern2 = re.compile("a_([0-9]*) ([0-9]*)")
+
+    for line in solver_output:
+        for match in re.finditer(pattern1, line):
+            instruction_position = int(match.group(1))
+            instruction_theta = match.group(2)
+            theta_sol[instruction_position] = int(instruction_theta)
+
+        for match in re.finditer(pattern2, line):
+            instruction_position = int(match.group(1))
+            pushed_value = match.group(2)
+            pushed_values_decimal[instruction_position] = pushed_value
+
+    return generate_dict_with_instructions(theta_sol, pushed_values_decimal)
